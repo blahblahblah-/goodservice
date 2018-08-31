8704280e3bbd348b447507d2461cd2b71b9b4af9
Sunny Ng <sunny@harrys.com>
2018-08-31 00:59:08 -0400

    Fix bug with not querying scheduled stop times correctly towards the end/beginning of day

--- app/models/stop_time.rb
+++ app/models/stop_time.rb
@@ -3,9 +3,25 @@ class StopTime < ActiveRecord::Base
   belongs_to :stop, foreign_key: "stop_internal_id", primary_key: "internal_id"
 
   def self.soon
-    where("departure_time > ? and departure_time < ?",
-      Time.current - Time.current.beginning_of_day,
-      Time.current - Time.current.beginning_of_day + 40.minutes.to_i
-    ).joins(trip: :schedule).merge(Schedule.today)
+    if (Time.current + 40.minutes).to_date == Date.current.tomorrow
+      where("(departure_time > ? and departure_time < ?) or (departure_time > ? and departure_time < ?)",
+        Time.current - Time.current.beginning_of_day,
+        Time.current - Time.current.beginning_of_day + 40.minutes.to_i,
+        0,
+        Time.current - Time.current.beginning_of_day + 40.minutes.to_i % 86400
+      ).joins(trip: :schedule).merge(Schedule.today)
+    elsif Time.current.hour < 4
+      where("(departure_time > ? and departure_time < ?) or (departure_time > ? and departure_time < ?)",
+        Time.current - Time.current.beginning_of_day,
+        Time.current - Time.current.beginning_of_day + 40.minutes.to_i,
+        86400,
+        Time.current - Time.current.beginning_of_day + 86400
+      ).joins(trip: :schedule).merge(Schedule.today)
+    else
+      where("departure_time > ? and departure_time < ?",
+        Time.current - Time.current.beginning_of_day,
+        Time.current - Time.current.beginning_of_day + 40.minutes.to_i
+      ).joins(trip: :schedule).merge(Schedule.today)
+    end
   end
 end
