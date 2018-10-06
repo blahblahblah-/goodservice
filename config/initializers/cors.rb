c69b0bd61dc22c80073d73c5383c5a7b9349b4b9
Sunny Ng <sunny@harrys.com>
2018-10-06 17:36:50 -0400

    Update CORS to allow HTTP for now

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:7700', '/\Ahttps://www.theweekendest.com/'
+    origins 'localhost:7700', '/\Ahttps://www.theweekendest.com/', '/\Ahttp://www.theweekendest.com/'
     resource '*',
       headers: :any,
       methods: %i(get)
