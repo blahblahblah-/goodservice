efb38b2c12f54727b2523469bd1c7031f31eff5f
Sunny Ng <sunny@harrys.com>
2018-10-06 20:40:30 -0400

    CORS again

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Goodservice
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:7700', '/\Ahttps://www.theweekendest.com/', '/\Ahttp://www.theweekendest.com/'
+        origins '*'
         resource '*',
           headers: :any,
           methods: %i(get)
