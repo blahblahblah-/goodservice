9872bca6f66839cb72108d3dc0e85b078c32d00c
Sunny Ng <sunny@harrys.com>
2019-02-27 08:08:31 -0500

    Bump tweet frequency to 15 mins

--- lib/clock.rb
+++ lib/clock.rb
@@ -18,7 +18,7 @@ module Clockwork
     ScheduleProcessor.instance.refresh_data
     midTime = Time.current
     puts "Refreshed data in #{midTime - startTime} seconds"
-    ScheduleProcessor.headway_info(force_refresh: true, tweet_delays: Time.current.min % 30 == 0)
+    ScheduleProcessor.headway_info(force_refresh: true, tweet_delays: Time.current.min % 15 == 0)
     processTime = Time.current
     puts "Processed headway info in #{processTime - midTime} seconds"
     ScheduleProcessor.stats_info(force_refresh: true)
