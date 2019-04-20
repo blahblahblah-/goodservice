57cd0943a8e8f7e1c26e19a0abb84d17bec9c52e
Sunny Ng <sunny@harrys.com>
2019-04-19 22:21:35 -0400

    Change cleanup job to purge LineDirectionStatuses older than 8 days

--- lib/clock.rb
+++ lib/clock.rb
@@ -46,7 +46,7 @@ module Clockwork
     RouteStatus.where("created_at < ?", Date.current - 2.months).delete_all
     ActualTrip.where("created_at < ?", Date.current - 2.months).delete_all
     Delay.where("created_at < ?", Date.current - 2.months).delete_all
-    LineDirectionStatus.where("created_at < ?", Date.current - 2.months).delete_all
+    LineDirectionStatus.where("created_at < ?", Date.current - 10.days).delete_all
     endTime = Time.current
     puts "DB cleanup finished in #{endTime - startTime} seconds"
 
