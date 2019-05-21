d3a02d120003c43f23c28c8e0ef8965fde0534d3
Sunny Ng <sunny@harrys.com>
2019-05-20 21:38:46 -0400

    Add Transfer model

---
+++ app/models/transfer.rb
@@ -0,0 +1,5 @@
+class Transfer < ActiveRecord::Base
+  belongs_to :from_stop, class_name: "Stop", foreign_key: "from_stop_internal_id", primary_key: "internal_id"
+  belongs_to :to_stop, class_name: "Stop", foreign_key: "to_stop_internal_id", primary_key: "internal_id"
+
+end
